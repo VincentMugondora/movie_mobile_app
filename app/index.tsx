612b951e2a7994dafe2688c5041cea3b8c49d959
vincentmugondora <vinmugondora@gmail.com>
2025-04-30 14:45:02 +0200

    Remove unused onboarding and movie details components from the project

--- app/index.tsx
+++ app/index.tsx
@@ -5,8 +5,6 @@ export default function Index() {
   return (
     <View className="items-center justify-center flex-1 gap-3">
       <Text className="text-4xl font-bold text-dark-200">Welcome To My Movie App</Text>
-      <Link href='/onboarding' className="p-2 rounded-md text-light-200 bg-dark-300">Onboarding</Link>
-      <Link href='/movie/avengers' className="p-2 rounded-md text-light-200 bg-dark-300">Avanger Movie</Link>
     </View>
   );
 }
