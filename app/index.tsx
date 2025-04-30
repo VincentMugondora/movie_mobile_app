c6596fa1842debdff78fadad319b020d5d50b329
vincentmugondora <vinmugondora@gmail.com>
2025-04-30 13:36:39 +0200

    Fix typo in welcome message text in Index component

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="items-center justify-center flex-1">
-      <Text className="text-4xl font-bold primary">Wlecome To My Movie App</Text>
+      <Text className="text-4xl font-bold text-primary">Welcome To My Movie App</Text>
     </View>
   );
 }
