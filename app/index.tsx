9afafdffc13f0a3ad04e444f4ba158367c548d03
vincentmugondora <vinmugondora@gmail.com>
2025-04-30 13:27:12 +0200

    Add text styling to welcome message in Index component

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text>Wlecome To My Movie App</Text>
+      <Text className="text-4xl">Wlecome To My Movie App</Text>
     </View>
   );
 }
