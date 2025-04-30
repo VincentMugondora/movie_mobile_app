3d61683a7dc0ebf76344132c7c55290fe9bdbe60
vincentmugondora <vinmugondora@gmail.com>
2025-04-30 15:37:00 +0200

    Remove unused screen components from RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,8 +5,5 @@ export default function RootLayout() {
   return <Stack>
     <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     <Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
-    <Stack.Screen name="(tabs)/saved" options={{ headerShown: false }} />
-    <Stack.Screen name="(tabs)/profile" options={{ headerShown: false }} />
-    <Stack.Screen name="(tabs)/search" options={{ headerShown: false }} />
   </Stack>;
 }
