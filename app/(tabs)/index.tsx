6a854f4676e029e43be1e200340cdf1a53c7ee65
vincentmugondora <vinmugondora@gmail.com>
2025-05-01 13:14:23 +0200

    Refactor Index component to enhance ScrollView properties for improved layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,8 +7,10 @@ export default function Index() {
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute z-0 w-full" />
-      <ScrollView className="flex-1 px-5">
-              <Image source={icons.logo} className="w-12 h-10 mx-auto mt-20 mb-5"/>
+      <ScrollView className="flex-1 px-5" showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
+        <Image source={icons.logo} className="w-12 h-10 mx-auto mt-20 mb-5"/>
       </ScrollView>
     </View>
   );
