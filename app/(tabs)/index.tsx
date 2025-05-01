f8b97440be2eab1f2892d0093c3ed6dfbc3ca81d
vincentmugondora <vinmugondora@gmail.com>
2025-05-01 12:57:44 +0200

    Remove welcome text from Index component for cleaner layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,8 +8,7 @@ export default function Index() {
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute z-0 w-full" />
       <ScrollView className="flex-1 px-5">
-        <Image source={icons.logo} className="w-12 h-10 mx-auto mt-20 mb-5"/>
-        <Text className="mb-5 text-3xl font-bold text-center text-white">Welcome to the App</Text>
+              <Image source={icons.logo} className="w-12 h-10 mx-auto mt-20 mb-5"/>
       </ScrollView>
     </View>
   );
