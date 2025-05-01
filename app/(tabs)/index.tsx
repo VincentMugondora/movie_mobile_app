be9811705d61fde1b10672b005f0e55a4c669636
vincentmugondora <vinmugondora@gmail.com>
2025-05-01 12:51:12 +0200

    Refactor Index component layout to enhance styling and add background image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,16 @@
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 import { Link } from "expo-router";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
 
 export default function Index() {
   return (
-    <View className="items-center justify-center flex-1 gap-3">
-      <Text className="text-4xl font-bold text-dark-200">Welcome To My Movie App</Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute z-0 w-full" />
+      <ScrollView className="flex-1 px-5">
+        <Image source={icons.logo} className="w-12 h-10 mx-auto mt-20 mb-5"/>
+        <Text className="mb-5 text-3xl font-bold text-center text-white">Welcome to the App</Text>
+      </ScrollView>
     </View>
   );
 }
